4114f32e51372cf58f5a5290e834d0b6f01da180
Karl Perkins <karl@karlperkins.me>
2024-01-14 12:00:47 +1300

    Add new grid functionality

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
 const grid = document.querySelector("#grid");
+const btn = document.querySelector("button");
 
-// 16 x 16 grid
-for (let i = 1; i <= 256; i++) {
-	const gridSquare = document.createElement("div");
-	gridSquare.classList.add("grid-square");
-	grid.appendChild(gridSquare);
+btn.addEventListener("click", () => {
+	const gridSize = prompt("Enter the number of squares per side.", 16);
+	createGrid(gridSize);
+});
+
+// Drawing
+grid.addEventListener(
+	"mouseenter",
+	(e) => {
+		const target = e.target;
+		if (target.classList.contains("grid-square")) {
+			target.classList.add("active");
+		}
+	},
+	true
+);
+
+// Initalise starting grid
+createGrid(16);
+
+function createGrid(gridSize) {
+	// Clear grid
+	for (let i = grid.childNodes.length; i > 0; i--) {
+		grid.removeChild(grid.lastChild);
+	}
+
+	// Create grid
+	for (let i = 0; i < gridSize * gridSize; i++) {
+		const gridSquare = document.createElement("div");
+		gridSquare.classList.add("grid-square");
+		const gridSquareSize = (1 / gridSize) * 100; // Percentage of grid height/width
+		gridSquare.setAttribute(
+			"style",
+			`height: ${gridSquareSize}%; flex: 0 1 ${gridSquareSize}%;`
+		);
+		grid.appendChild(gridSquare);
+	}
 }
-
-// Draw
-const gridSquares = document.querySelectorAll('.grid-square');
-
-gridSquares.forEach(square => square.addEventListener('mouseenter', () => { square.classList.add('active'); }));
